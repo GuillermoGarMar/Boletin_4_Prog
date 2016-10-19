52571fa63a14d489d09612847e99f17b7006cc85
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-10-19 08:42:57 +0200

    Merge origin/master
    Conflicts:
    	src/clonar/Clonar.java

---
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_3;
+
+/**
+ *
+ * @author ggarciamartinez
+ */
+public class Circulo {
+    private double radio;
+    private double PI = 3.14f;
+    public Circulo(){
+    }
+    public Circulo(double rad){
+            radio = rad;
+    }
+    public void setR(double r){
+        radio=r;
+    }
+    public double getR(){
+        return radio;
+    }
+    public double calcA(){
+        return Math.pow(radio, 2)*PI;
+    }
+    public double calcL(){
+        return radio*2;
+    }
+}
