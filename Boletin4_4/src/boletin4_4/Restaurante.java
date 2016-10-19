f4791b61d6b3714e76fa8dc0945af7b41c76502b
ggarciamartinez <ggarciamartinez@atanasoff20.danielcastelao.org>
2016-10-19 08:43:06 +0200

    Merge origin/master
    Conflicts:
    	src/clonar/Clonar.java

---
+++ Boletin4_4/src/boletin4_4/Restaurante.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_4;
+
+/**
+ *
+ * @author ggarciamartinez
+ */
+public class Restaurante {
+    private int KgP;
+    private int KGpat;
+    
+     public void setPulp(int x ){
+         KgP = x;
+     }
+     
+     public void setPat ( int x)     ''           
+   public void getPulp( )
+   public void getPat ( )
+}
